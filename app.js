744d148a6c1935566fe92ef68a4bef16eaa94382
AndreEva <app.re.app@icloud.com>
2023-10-19 11:53:57 +0300

    upload and pagination

--- app.js
+++ app.js
@@ -1,59 +1,54 @@
 var dataURL = 'https://jsonplaceholder.typicode.com/posts?_limit=20';
 
-
-
-Vue.component('paginated-list',{
-data(){
-return {
-  pageNumber: 0
-}
-},
-props:{
-listData:{
-  type:Array,
-  required:true
-},
-size:{
-  type:Number,
-  required:false,
-  default: 6
-}
-},
-methods:{
-  nextPage(){
-     this.pageNumber++;
+Vue.component('paginated-list', {
+  data() {
+    return {
+      pageNumber: 0
+    }
   },
-  prevPage(){
-    this.pageNumber--;
+  props: {
+    listData: {
+      type: Array,
+      required: true
+    },
+    size: {
+      type: Number,
+      required: false,
+      default: 6
+    }
   },
-  selectPage(page){
-    this.pageNumber=page;
-  }
-
-
-},
-computed:{
-pageCount(){
-  let l = this.listData.length,
-      s = this.size;
-  return Math.ceil(l/s);
-},
-paginatedData(){
-  const start = this.pageNumber * this.size,
+  methods: {
+    nextPage() {
+      this.pageNumber++;
+    },
+    prevPage() {
+      this.pageNumber--;
+    },
+    selectPage(page) {
+      this.pageNumber = page;
+    }
+  },
+  computed: {
+    pageCount() {
+      let l = this.listData.length,
+        s = this.size;
+      return Math.ceil(l / s);
+    },
+    paginatedData() {
+      const start = this.pageNumber * this.size,
         end = start + this.size;
-  return this.listData
-           .slice(start, end);
-}
-},
-template: `<div>
+      return this.listData
+        .slice(start, end);
+    }
+  },
+  template: `<div>
            
-
-           <div class="row" v-for="p in paginatedData">
+           <div v-for="p in paginatedData">
            <div class="article col">
                <h3 class="title">{{p.title}}</h3>
                <p class="body">{{p.body}}</p>
            </div>
-</div>
+           </div>
           
           <div class="pagination">
           <button class="pagination__button"
@@ -79,6 +74,7 @@ template: `<div>
               &#62;&#62;
           </button>
         </div>
+
         </div>
          
          
@@ -86,60 +82,39 @@ template: `<div>
 });
 
 
-function createFakeData(){
-let data = [];
-for(let i = 0; i < 100; i++){
-data.push({first: 'John',
-           last:'Doe', 
-           suffix:'#' + i});
-}
-return data;
-}
-
-
-
 var app = new Vue({
-    el: "#app",
-    data: {
-        newtitle:'',
-        newbody:'',
-        pageNumber: 0,
-        articles: [],
-        
+  el: "#app",
+  data: {
+    newtitle: '',
+    newbody: '',
+    title: '',
+    body: '',
+    pageNumber: 0,
+    articles: [],
 
-    },
-    methods: {
-        // пагинация
-        // nextPage() {
-        //     this.pageNumber++;
-        // },
-        // prevPage() {
-        //     if (this.pageNumber != 0) {
-        //         this.pageNumber--;
-        //     }
-        // },
-        //Добвление статей
-        addArticle(newtitle, newbody) {
-            app.articles.push({
-                title: newtitle,
-                body: newbody
-            });
-           
-            
 
-        }
+  },
+  methods: {
+    addArticle(newtitle, newbody) {
+      app.articles.push({
+        title: newtitle,
+        body: newbody
+      });
+
+      this.title = ''
+      this.body = ''
     },
 
-    //Загрузка json
-    mounted() { // when the Vue app is booted up, this is run automatically.
-        var self = this // create a closure to access component in the callback below
-        $.getJSON(dataURL, function (data) {
-            self.articles = data;
-        });
-    },
 
-    
+  },
 
+  //Загрузка json
+  mounted() { // when the Vue app is booted up, this is run automatically.
+    var self = this // create a closure to access component in the callback below
+    $.getJSON(dataURL, function (data) {
+      self.articles = data;
+    });
+  },
 
 
 });
